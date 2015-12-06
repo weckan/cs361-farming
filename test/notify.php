73b2660fab1daebfbdfc42461d4a82010dbd0267
Richard Ratliff <ratlifri@onid.oregonstate.edu>
2015-12-06 10:51:17 -0800

    updated notification message

--- test/notify.php
+++ test/notify.php
@@ -15,7 +15,7 @@ error_reporting(E_ALL);
     $subject = "Farm Alert";
     $txt = "<p>Hello,</p>";
     $txt = "<p>This is an alert from your Home Farm Management system.</p>";
-    $txt .= "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>";
+    $txt .= "<p>There is a problem with the water levels in your system. Please check your equipment at your earliest convenience.</p>";
     $txt = wordwrap($txt,70);
     $headers = "MIME-Version: 1.0" . "\r\n";
     $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
